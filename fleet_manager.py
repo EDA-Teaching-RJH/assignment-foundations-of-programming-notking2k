b57e630bc8fb33272ff070b1e5caf9d95f560d41
notking2k <kkm@kent.ac.uk>
2026-02-16 09:32:13 +0000

    i created the file name and also decided to add the initial database  with the crew lists

---
+++ fleet_manager.py
@@ -0,0 +1,6 @@
+def init_database():
+    names = ["Picard" "Riker","Data","Worf","Crusher"]
+    ranks = ["Captain","Commander,"Lt.Commander","lieutenant","ensign"]
+    divs = ["command","operations","operations","sciences"]
+    ids = ["001","002","003","004","005"]
+    return names, ranks, divs, ids
